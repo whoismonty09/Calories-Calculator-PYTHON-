53c079028629c55c0427da0aa9d222a2734c4cab
Mayank malviya <mayankmalviya440@gmail.com>
2026-02-27 01:19:10 +0530

    Update Calorie.py

--- Calorie.py
+++ Calorie.py
@@ -1,1 +1,12 @@
+print("Calorie Calculator developed by Monty")
+weight = float(input("Enter weight in kg: "))
+height = float(input("Enter height in cm: "))
+age = int(input("Enter age: "))
+gender = input("Enter gender (male/female): ").lower()
 
+if gender == "male":
+    calories = 10 * weight + 6.25 * height - 5 * age + 5
+else:
+    calories = 10 * weight + 6.25 * height - 5 * age - 161
+
+print("Daily Calories Needed:", int(calories))
